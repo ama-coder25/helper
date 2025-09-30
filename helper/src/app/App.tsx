401e8f34c8b1081c409068b0c6f7838ef3c16f55
ama-coder25 <ama.jama@yandex.ru>
2025-09-30 11:05:39 +0300

    Update App.tsx

--- helper/src/app/App.tsx
+++ helper/src/app/App.tsx
@@ -1,12 +1,16 @@
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/homePage/homePage";
 
-
 function App() {
-     return <HomePage /> 
-};
-    
-
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Если потом добавишь новые страницы, просто добавь Route */}
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
-

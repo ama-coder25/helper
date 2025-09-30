62592771970f29bf5c2b8416fd993292ad33a312
ama-coder25 <ama.jama@yandex.ru>
2025-09-30 11:02:58 +0300

    Update vite.config.ts

--- helper/vite.config.ts
+++ helper/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss() ],
+  base: '/helper/', // <-- добавь эту строку
+  plugins: [react(), tailwindcss()],
 })
